30992d8dcbeed19d1792cf63c61ed75ae5596a43
James Herdman <james.herdman@gmail.com>
2017-08-10 18:55:26 +0000

    Critical Fixes Required for Release

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'date-picker'
+  name: 'date-picker',
+
+  contentFor(type, config) {
+    // https://github.com/cibernox/ember-power-select/issues/145#issuecomment-170012836
+    if (type === 'body-footer') {
+      let emberPowerDatepicker = this.addons.filter(function(addon) {
+        return addon.name === 'ember-power-datepicker';
+      })[0];
+
+      return emberPowerDatepicker.contentFor(type, config);
+    } else {
+      return '';
+    }
+  },
 };
