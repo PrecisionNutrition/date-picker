20456e782af1ef4d1a52de0764d0376b9b53d083
James Herdman <james.herdman@gmail.com>
2019-11-01 20:00:23 +0000

    Lint the Templates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,14 @@
 
 module.exports = {
   extends: 'recommended',
+
+  // See https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rules.md
+  rules: {
+    'attribute-indentation': true,
+    'eol-last': 'always',
+    'no-invalid-interactive': true,
+    'no-duplicate-attributes': true,
+    'no-trailing-spaces': true,
+    'style-concatenation': true,
+  },
 };
